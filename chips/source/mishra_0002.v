4338a8b78e782f31bd66fe59ef2092d9f60bd4b4
pervigilent <pervigilent@gmail.com>
2026-01-11 10:42:35 -0700

    Changed tabs to spaces

--- chips/source/mishra_0002.v
+++ chips/source/mishra_0002.v
@@ -1,21 +1,21 @@
 module xmit_b (
-	xmit_b_in1,
-	xmit_b_in2,
-	xmit_b_in3,
-	xmit_b_out1,
-	xmit_b_out2
+    xmit_b_in1,
+    xmit_b_in2,
+    xmit_b_in3,
+    xmit_b_out1,
+    xmit_b_out2
 );
-	// --------------------
-	input [7:0] xmit_b_in1;
-	input xmit_b_in2;
-	input xmit_b_in3;
-	output xmit_b_out1;
-	output xmit_b_out2;
-	// --------------------
-	// Synthesizable RTL code
-	always @(*) begin
-		// -----
-		// -----
-	end
+    // --------------------
+    input [7:0] xmit_b_in1;
+    input xmit_b_in2;
+    input xmit_b_in3;
+    output xmit_b_out1;
+    output xmit_b_out2;
+    // --------------------
+    // Synthesizable RTL code
+    always @(*) begin
+        // -----
+        // -----
+    end
 end module
 
